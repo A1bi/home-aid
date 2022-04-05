7da85889294b1948d98a29130771f05961362496
Albrecht Oster <albrecht@oster.online>
2022-04-05 10:50:50 +0200

    Rewrite HomeMatic client in ES6

--- HomeMatic.js
+++ HomeMatic.js
@@ -1,12 +1,9 @@
-'use strict';
+const EventEmitter = require('events').EventEmitter
+const rpc = require('binrpc')
+const storage = require('node-persist')
+const HomeMaticDevice = require('./HomeMaticDevice')
 
-var EventEmitter = require('events').EventEmitter;
-var rpc = require('binrpc');
-var storage = require('node-persist');
-
-var HomeMaticDevice = require('./HomeMaticDevice');
-
-var hosts = {
+const hosts = {
   self: {
     host: 'localhost',
     port: 2002
@@ -15,161 +12,162 @@ var hosts = {
     host: 'localhost',
     port: 2001
   }
-};
+}
 
-var rpcServer, rpcClient;
-var subscriptionUrl = 'xmlrpc_bin://' + hosts.self.host + ':' + hosts.self.port;
+var rpcServer, rpcClient
+const subscriptionUrl = `xmlrpc_bin://${hosts.self.host}:${hosts.self.port}`
 
-var supportedDevices = ['CLIMATECONTROL_RT_TRANSCEIVER', 'SMOKE_DETECTOR'];
-var managedDevices = {};
+const supportedDevices = ['CLIMATECONTROL_RT_TRANSCEIVER', 'SMOKE_DETECTOR']
+const managedDevices = {}
 
-var updateInterfaceClockTimer;
-var emitter = new EventEmitter();
+var updateInterfaceClockTimer
+const emitter = new EventEmitter()
 
+function registerEvents () {
+  rpcServer.on('system.listMethods', (err, params, callback) => {
+    if (err) return console.log(err)
 
-function registerEvents() {
-  rpcServer.on('system.listMethods', function (err, params, callback) {
-    callback(null, ['system.listMethods', 'system.multicall', 'event', 'listDevices']);
-  });
+    callback(null, ['system.listMethods', 'system.multicall', 'event', 'listDevices'])
+  })
 
-  rpcServer.on('listDevices', function (err, params, callback) {
-    storage.getItem('devices').then(function (devices) {
-      var list = (devices || []).map(function (device) {
-        return { ADDRESS: device.ADDRESS, VERSION: device.VERSION };
-      });
-      callback(null, list);
-    });
-  });
+  rpcServer.on('listDevices', (err, params, callback) => {
+    if (err) return console.log(err)
 
-  rpcServer.on('event', function (err, params, callback) {
-    var addr = getParentAddress(params[1]);
-    var device = managedDevices[addr];
+    storage.getItem('devices').then(devices => {
+      const list = (devices || []).map(device => {
+        return { ADDRESS: device.ADDRESS, VERSION: device.VERSION }
+      })
+      callback(null, list)
+    })
+  })
+
+  rpcServer.on('event', (err, params, callback) => {
+    if (err) return console.log(err)
+
+    const addr = getParentAddress(params[1])
+    const device = managedDevices[addr]
     if (device) {
-      device.applyUpdate(params[2], params[3]);
+      device.applyUpdate(params[2], params[3])
     }
-    callback();
-  });
+    callback()
+  })
 
-  rpcServer.on('newDevices', function (err, params, callback) {
-    var newDevices = params[1];
+  rpcServer.on('newDevices', (err, params, callback) => {
+    if (err) return console.log(err)
+
+    const newDevices = params[1]
 
     storage.getItem('devices')
-      .then(function (oldDevices) {
-        newDevices.forEach(function (newDevice) {
-          var isNew = oldDevices.every(function (oldDevice, index) {
+      .then(oldDevices => {
+        for (var newDevice of newDevices) {
+          const isNew = oldDevices.every((oldDevice, index) => {
             if (oldDevice.ADDRESS === newDevice.ADDRESS) {
-              Object.assign(oldDevices[index], newDevice);
-              return false;
+              Object.assign(oldDevices[index], newDevice)
+              return false
             }
-            return true;
-          });
+            return true
+          })
 
           if (isNew) {
-            oldDevices.push(newDevice);
-            addDevices([newDevice]);
+            oldDevices.push(newDevice)
+            addDevices([newDevice])
           }
-        });
+        }
 
-        return storage.setItem('devices', oldDevices);
+        return storage.setItem('devices', oldDevices)
       })
-    ;
 
-    callback();
-  });
+    callback()
+  })
 
-  rpcClient.on('connect', function () {
-    console.log('Connected to HomeMatic RFD RPC server.');
+  rpcClient.on('connect', () => {
+    console.log('Connected to HomeMatic RFD RPC server.')
 
-    updateInterfaceClock();
-    clearTimeout(updateInterfaceClockTimer);
-    updateInterfaceClockTimer = setInterval(updateInterfaceClock, 3600000);
+    updateInterfaceClock()
+    clearTimeout(updateInterfaceClockTimer)
+    updateInterfaceClockTimer = setInterval(updateInterfaceClock, 3600000)
 
-    subscribe();
-  });
+    subscribe()
+  })
 }
 
-function addDevices(devices) {
-  devices.forEach(function (deviceInfo) {
+function addDevices (devices) {
+  for (var deviceInfo of devices) {
     if (supportedDevices.indexOf(deviceInfo.TYPE) > -1) {
-      var addr = getParentAddress(deviceInfo.ADDRESS);
-      var newDevice = managedDevices[addr] = new HomeMaticDevice(deviceInfo.TYPE, deviceInfo.ADDRESS, methodCall);
-      emitter.emit('newDevice', newDevice);
+      const addr = getParentAddress(deviceInfo.ADDRESS)
+      const newDevice = managedDevices[addr] = new HomeMaticDevice(deviceInfo.TYPE, deviceInfo.ADDRESS, methodCall)
+      emitter.emit('newDevice', newDevice)
     }
-  });
-}
-
-function methodCall(method, params, callback) {
-  rpcClient.methodCall(method, params, function (err, res) {
-    if (err) {
-      console.log(err);
-    }
-
-    if (callback) {
-      callback(err, res);
-    }
-  });
-}
-
-function updateInterfaceClock() {
-  var timestamp = parseInt(Date.now() / 1000);
-  var offset = new Date().getTimezoneOffset() * -1;
-  methodCall('setInterfaceClock', [timestamp, offset]);
-}
-
-function subscribe() {
-  methodCall('init', [subscriptionUrl, 'foo'], function (err, res) {
-    console.log('Subscribed to HomeMatic events.');
-
-    storage.getItem('devices').then(function (devices) {
-      addDevices(devices);
-      emitter.emit('ready');
-    });
-  });
-}
-
-function unsubscribe(callback) {
-  methodCall('init', [subscriptionUrl, ''], function (err, res) {
-    console.log('Unsubscribed from HomeMatic events.');
-    if (callback) {
-      callback();
-    }
-  });
-}
-
-function togglePairing(toggle, seconds) {
-  toggle = !!toggle;
-  seconds = seconds || 60;
-  methodCall('setInstallMode', [toggle, seconds, 1], function (err, res) {
-    if (!err) {
-      console.log((toggle ? 'Enabled' : 'Disabled') + ' pairing for ' + seconds + ' seconds.');
-    }
-  });
-}
-
-function getParentAddress(address) {
-  return address.split(':')[0];
-}
-
-function init() {
-  storage.init();
-
-  if (!rpcServer && !rpcClient) {
-    rpcServer = rpc.createServer(hosts.self);
-    rpcClient = rpc.createClient(hosts.rfd);
-    registerEvents();
   }
 }
 
-function exit(callback) {
-  clearTimeout(updateInterfaceClockTimer);
-  unsubscribe(callback);
+function methodCall (method, params, callback) {
+  rpcClient.methodCall(method, params, (err, res) => {
+    if (err) console.log(err)
+    else if (callback) callback(err, res)
+  })
+}
+
+function updateInterfaceClock () {
+  const timestamp = parseInt(Date.now() / 1000)
+  const offset = new Date().getTimezoneOffset() * -1
+  methodCall('setInterfaceClock', [timestamp, offset])
+}
+
+function subscribe () {
+  methodCall('init', [subscriptionUrl, 'foo'], (err, res) => {
+    if (err) return console.log(err)
+
+    console.log('Subscribed to HomeMatic events.')
+
+    storage.getItem('devices').then(devices => {
+      addDevices(devices)
+      emitter.emit('ready')
+    })
+  })
+}
+
+function unsubscribe (callback) {
+  methodCall('init', [subscriptionUrl, ''], (err, res) => {
+    if (err) return console.log(err)
+
+    console.log('Unsubscribed from HomeMatic events.')
+    if (callback) callback()
+  })
+}
+
+function togglePairing (toggle, seconds) {
+  toggle = !!toggle
+  seconds = seconds || 60
+  methodCall('setInstallMode', [toggle, seconds, 1], (err, res) => {
+    if (!err) {
+      console.log(`${toggle ? 'Enabled' : 'Disabled'} pairing for ${seconds} seconds.`)
+    }
+  })
+}
+
+function getParentAddress (address) {
+  return address.split(':')[0]
+}
+
+function init () {
+  storage.init()
+
+  if (!rpcServer && !rpcClient) {
+    rpcServer = rpc.createServer(hosts.self)
+    rpcClient = rpc.createClient(hosts.rfd)
+    registerEvents()
+  }
+}
+
+function exit (callback) {
+  clearTimeout(updateInterfaceClockTimer)
+  unsubscribe(callback)
 }
 
 module.exports = {
   init: init,
   exit: exit,
   togglePairing: togglePairing,
-  on: function (name, cb) {
-    emitter.on(name, cb);
-  }
-};
+  on: (name, cb) => emitter.on(name, cb)
+}
--- HomeMaticDevice.js
+++ HomeMaticDevice.js
@@ -1,62 +1,59 @@
-'use strict';
+const EventEmitter = require('events').EventEmitter
 
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
+class HomeMaticDevice extends EventEmitter {
+  constructor (type, address, methodCall) {
+    super()
 
-var HomeMatic = require('./HomeMatic');
+    this.type = type
+    this.address = address
+    this.methodCall = methodCall
+    this.characteristics = []
+    this.values = {}
 
-module.exports = HomeMaticDevice;
+    this.methodCall('getParamset', [this.address, 'VALUES'], (err, res) => {
+      if (err) return console.log(err)
 
-function HomeMaticDevice(type, address, methodCall) {
-  this.type = type;
-  this.address = address;
-  this.methodCall = methodCall;
-  this.characteristics = [];
-  this.values = {};
+      for (var characteristic in res) {
+        this.updateValue(characteristic, res[characteristic])
+      }
+      this.emit('ready')
+    })
+  }
 
-  this.methodCall('getParamset', [this.address, 'VALUES'], function (err, res) {
-    for (var characteristic in res) {
-      this.updateValue(characteristic, res[characteristic]);
+  getValue (characteristic) {
+    return this.values[characteristic]
+  }
+
+  setValue (characteristic, value, callback) {
+    console.log('setting value', characteristic, value)
+    if (typeof value === 'number') {
+      value = value.toFixed(1)
     }
-    this.emit('ready');
-  }.bind(this));
+    this.methodCall('setValue', [this.address, characteristic, value], (err) => callback(err))
+  }
+
+  updateValue (characteristic, value) {
+    if (characteristic === 'BATTERY_STATE') {
+      value = parseInt((value - 1.5) / 3.1 * 100)
+    }
+
+    if (this.values[characteristic] !== value) {
+      this.values[characteristic] = value
+      return true
+    }
+
+    return false
+  }
+
+  applyUpdate (characteristic, value) {
+    if (this.updateValue(characteristic, value)) {
+      this.emit('update', characteristic, value)
+    }
+  }
+
+  getCharacteristics () {
+    return Object.keys(this.values)
+  }
 }
 
-util.inherits(HomeMaticDevice, EventEmitter);
-
-HomeMaticDevice.prototype.getValue = function (characteristic) {
-  return this.values[characteristic];
-};
-
-HomeMaticDevice.prototype.setValue = function (characteristic, value, callback) {
-  console.log('setting value', characteristic, value);
-  if (typeof value === 'number') {
-    value = value.toFixed(1);
-  }
-  this.methodCall('setValue', [this.address, characteristic, value], function (err) {
-    callback(err);
-  });
-};
-
-HomeMaticDevice.prototype.updateValue = function (characteristic, value) {
-  if (characteristic === 'BATTERY_STATE') {
-    value = parseInt((value - 1.5) / 3.1 * 100);
-  }
-
-  if (this.values[characteristic] !== value) {
-    this.values[characteristic] = value;
-    return true;
-  }
-
-  return false;
-};
-
-HomeMaticDevice.prototype.applyUpdate = function (characteristic, value) {
-  if (this.updateValue(characteristic, value)) {
-    this.emit('update', characteristic, value);
-  }
-};
-
-HomeMaticDevice.prototype.getCharacteristics = function () {
-  return Object.keys(this.values);
-};
+module.exports = HomeMaticDevice
